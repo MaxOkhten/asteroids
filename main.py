1799f4fd3440adddce2a6ada381fe4a08d7c5bad
MaxOkhten <maxokhten@gmail.com>
2025-12-11 22:49:03 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,61 +1,64 @@
 import pygame
-import constants
-import player
-import asteroid
 import sys
 
+from constants import *
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
 from asteroidfield import AsteroidField
-from logger import log_state
-from logger import log_event
 
 def main():
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
 
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
-    clock_object = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    player.Player.containers = (updatable, drawable)
-    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
 
-    AsteroidField.containers = (updatable,)
-    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
-    new_player = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
 
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print (f"Screen width: {constants.SCREEN_WIDTH}")
-    print (f"Screen height: {constants.SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # new_player.update(dt)
+        screen.fill("black")
+
         updatable.update(dt)
 
         for rock in asteroids:
-            if rock.collides_with(new_player):
+            if rock.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
-                sys.exit()
+                sys.exit()    
 
-        screen.fill("black")
+        for element in drawable:
+            element.draw(screen)
 
-        for obj in drawable:
-            obj.draw(screen)
+        # player.update(dt)
+        # player.draw(screen)
 
         pygame.display.flip()
 
-        dt = clock_object.tick(60) / 1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
